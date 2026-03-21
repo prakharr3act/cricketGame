bdde877143f4d7f49e1d5726109c1c34c7e99e7d
Prakharr3act <prakhar.codes433@gmail.com>
2026-03-21 20:46:09 +0530

    add version 4

---
+++ cricketGameV4/cg.js
@@ -0,0 +1,68 @@
+let userChoice;
+let botChoice;
+
+const ResultText = document.querySelector("p");
+const ResultArray = document.querySelector("h5");
+
+function batChoice(){
+  userChoice = "bat";
+  botChoiceGenerate();
+  displayResult();
+}
+
+function ballChoice(){
+  userChoice = "ball";
+  botChoiceGenerate();
+  displayResult();
+}
+
+function stumpChoice(){
+  userChoice = "stump";
+  botChoiceGenerate();
+  displayResult();
+}
+
+function botChoiceGenerate(){
+  let randomNum = Math.floor(Math.random()*3);
+
+  if(randomNum === 0){
+    botChoice = "bat";
+  }
+  else if (randomNum === 1){
+    botChoice = "ball";
+  }
+  else{
+    botChoice = "stump";
+  }
+}
+
+let Score = {
+  win: 0,
+  lost: 0,
+  tie: 0,
+  displayScore(){
+      ResultArray.innerText = `[Win:${Score.win}| Lost: ${Score.lost} |Tie:${Score.tie}]`
+  }
+}
+
+function displayResult(){
+  let result = "";
+
+  if(userChoice === botChoice){
+    result = "It's a Tie!";
+    Score.tie++;
+  } else if(
+    (userChoice === "bat" && botChoice === "ball") ||
+    (userChoice === "ball" && botChoice === "stump") ||
+    (userChoice === "stump" && botChoice === "bat")
+  ){
+    result = "You Win!";
+    Score.win++;
+  } else {
+    result = "Bot Wins!";
+    Score.lost++;
+  }
+
+  ResultText.innerText = `You: ${userChoice} | Bot: ${botChoice} | ${result}`;
+   Score.displayScore();
+}
